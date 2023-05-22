3effe72ebbf1fc6050fe4fd713b11e8e667a77f1
Agung <muhammadagunghartono@gmail.com>
2023-05-22 16:54:19 +0700

    update web ljn

--- js/main.js
+++ js/main.js
@@ -107,3 +107,20 @@
     
 })(jQuery);
 
+ // Gauge
+ const gaugeElement = document.querySelector(".gauge");
+
+ function setGaugeValue(gauge, value) {
+     if (value < 0 || value > 1) {
+         return;
+     }
+
+     gauge.querySelector(".gauge__fill").style.transform = `rotate(${
+         value / 2
+     }turn)`;
+     gauge.querySelector(".gauge__cover").textContent = `${Math.round(
+         value * 100
+     )}Mbps`;
+ }
+
+ setGaugeValue(gaugeElement, 0.1);
