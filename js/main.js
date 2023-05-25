c992894ef0fe7fe67d2a2e73784833d1d3c34bc1
Agung <muhammadagunghartono@gmail.com>
2023-05-25 14:19:17 +0700

    update web ljn

--- js/main.js
+++ js/main.js
@@ -122,4 +122,21 @@
 // year
 document.getElementById("footer-year").innerHTML = new Date().getFullYear();
 
-// speedometer
+// menu mobile
+$('.toggle-mobile').on('click', function() {
+$('.menu-mobile').toggleClass('expanded');  
+$('.span-mobile').toggleClass('hidden');  
+$('.container-mobile , .toggle-mobile').toggleClass('close');  
+});
+
+// function myFunction(x) {
+//     if (x.matches) { // If media query matches
+      
+//     } else {
+//       document.body.style.backgroundColor = "pink";
+//     }
+//   }
+  
+//   var x = window.matchMedia("(max-width: 700px)")
+//   myFunction(x) // Call listener function at run time
+//   x.addListener(myFunction) // Attach listener function on state changes 
